14756326e316e7883df2c0ea310f52d83fc7fdf5
Rory Tanner <roryscoding@gmail.com>
2021-02-09 21:07:25 +1100

    deleted attempted admincard, built base code for attendance and feedback forms, installed packages emailjs-com. Added code for button submit to trigger email being sent.

--- client/src/components/Attendance/Attendance.js
+++ client/src/components/Attendance/Attendance.js
@@ -1,9 +1,32 @@
 import React from 'react';
-//import styled from 'styled-components';
+import emailjs from 'emailjs-com';
 
+//import './Attendance.css';
 
-const Attendance = () => {
-    return <div>Register Attendance</div>
+export default function Attendance() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_f2mja5e', 'template_10fz4sm', e.target, 'user_eYRgicD7vsNuF9O62tN6C')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      
+      <input type="text" name="user_name" placeholder='Enter name'/> <br></br>
+      
+      <input type="email" name="user_email" placeholder='Enter email'/> <br></br>
+      
+      <textarea name="comments" placeholder='Enter comments or questions here'/> <br></br>
+      <br></br>
+      <input type="submit" value="Register your Attendance at Training" />
+    </form>
+  );
 }
-export default Attendance
-
--- client/src/components/Feedback/Feedback.js
+++ client/src/components/Feedback/Feedback.js
@@ -1,8 +1,32 @@
 import React from 'react';
-//mport styled from 'styled-components';
+import emailjs from 'emailjs-com';
 
+//import './Feedback.css';
 
-const Feedback = () => {
-    return <div>Complete Feedback</div>
+export default function Feedback() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_f2mja5e', 'template_zigik5o', e.target, 'user_eYRgicD7vsNuF9O62tN6C')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      
+      <input type="text" name="user_name" placeholder='Enter name'/> <br></br>
+      
+      <input type="email" name="user_email" placeholder='Enter email'/> <br></br>
+      
+      <textarea name="comments" placeholder='Enter comment / feedback'/> <br></br>
+      <br></br>
+      <input type="submit" value="Send your feedback" />
+    </form>
+  );
 }
-export default Feedback
